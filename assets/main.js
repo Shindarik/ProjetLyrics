44d661c6466dbeb294915242709b9486164b963b
SUSLU Matéo <78606237+Shindarik@users.noreply.github.com>
2021-05-17 19:31:24 +0200

    up

--- assets/main.js
+++ assets/main.js
@@ -15,10 +15,12 @@ function ElementVue(e) {
 window.addEventListener("scroll", function (e) {
 
     if (ElementVue(navCheck) == true) {
-        nav.style.backgroundColor = "rgba(17, 17, 17, 1)";
+        nav.style.backgroundImage = "url(../medias/double-bubble-dark.png)";
+        nav.style.backgroundColor = "orangered";
         nav.style.borderBottom = "groove rgb(0, 121, 202) 5px";
     } else {
-        nav.style.backgroundColor = "rgba(17, 17, 17, 0)";
-        nav.style.borderBottom = "groove rgb(0, 121, 202) 0px";
+        nav.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        nav.style.backgroundImage = "none";
+        nav.style.borderBottom = "none";
     }
 });
